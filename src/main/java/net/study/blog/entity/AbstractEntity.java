b4796ed2a45801ba02b70566bffee4f1bb6e9d0c
Ihor Krat <ihor.krat@gmail.com>
2016-10-01 19:14:07 +0300

    entities and model

---
+++ src/main/java/net/study/blog/entity/AbstractEntity.java
@@ -0,0 +1,46 @@
+package net.study.blog.entity;
+
+import java.io.Serializable;
+
+import net.study.blog.model.AbstractModel;
+
+public abstract class AbstractEntity<PK> extends AbstractModel implements Serializable {
+	private static final long serialVersionUID = -568505210584945752L;
+
+	private PK id;
+
+	public PK getId() {
+		return id;
+	}
+
+	public void setId(PK id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AbstractEntity other = (AbstractEntity) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
---
+++ src/main/java/net/study/blog/entity/Account.java
@@ -0,0 +1,43 @@
+package net.study.blog.entity;
+
+import java.sql.Timestamp;
+
+public class Account extends AbstractEntity<Long> {
+	private static final long serialVersionUID = 8586641277064798847L;
+	private String email;
+	private String name;
+	private String avatar;
+	private Timestamp created;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAvatar() {
+		return avatar;
+	}
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+	public Timestamp getCreated() {
+		return created;
+	}
+	public void setCreated(Timestamp created) {
+		this.created = created;
+	}
+	
+	public boolean isAvatarExists(){
+		return avatar != null;
+	}
+	
+	public String getNoAvatar(){
+		return "/static/img/no_avatar.png";
+	}
+}
